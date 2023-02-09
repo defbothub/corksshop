5da436bae45772decddec97a7181b40ca8e2c54e
Your Name <defbothub@gmail.com>
2023-02-09 09:07:36 +0200

    Update requirements.txt

--- data/config.py
+++ data/config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = '5977438779:AAHWz2e0PGt62uZj_Hyq6PSeW_1E3lz92wo'
 
-ADMINS = [672637518]
+ADMINS = [5791823682]
 
 sale_photo = 'https://drive.google.com/file/d/1Sx7N6yZvxKZ1vbyid_VHZYfbx_9bcdkZ/view?usp=sharing'
 adress = 'https://drive.google.com/file/d/1zGSWoX29hy2mZF3wvkTRg-kLe2GrFuWQ/view?usp=sharing'
